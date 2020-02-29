f89991941574b462f291d93e19806146b05a99f5
Eric Norman <eric.norman98@hotmail.se>
2020-02-29 15:19:43 +0100

    Improvements on using NSynth with GANs

--- src/nsynth/nsynth.py
+++ src/nsynth/nsynth.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 import numpy as np
+import sys
 import librosa
 import matplotlib.pyplot as plt
 
@@ -25,74 +26,122 @@ class NSynthDataset():
                                           num_parallel_calls=tf.data.experimental.AUTOTUNE)
         return self
 
+    def one_hot(self, index, depth):
+        self.examples = self.examples.map(self._map_index(index, lambda x: tf.one_hot(x, depth)),
+                                          num_parallel_calls=tf.data.experimental.AUTOTUNE)
+        return self
+
     def filter(self, predicate):
         self.examples = self.examples.filter(predicate)
         return self
 
-    def audio(self):
-        self.examples = self.examples.map(lambda x: tf.reshape(x['audio'], [-1, 1]),
+    def extract(self, keys):
+        self.examples = self.examples.map(lambda x: [x[key] for key in keys],
                                           num_parallel_calls=tf.data.experimental.AUTOTUNE)
         return self
 
-    def audio_padstart(self, timesteps):
-        self.examples = self.examples.map(self._padstart(timesteps),
+    def audio_padstart(self, index, timesteps):
+        self.examples = self.examples.map(self._padstart(index, timesteps),
                                           num_parallel_calls=tf.data.experimental.AUTOTUNE)
         return self
 
-    def _padstart(self, timesteps):
+    def _padstart(self, index, timesteps):
         def pad(x):
             padding = tf.random.normal([timesteps, 1])
-            return tf.concat([padding, x], 0)
+            x[index] = tf.concat([padding, x[index]], 0)
+            return x
         return pad
 
-    def audio_windows(self, timesteps):
-        self.examples = self.examples.map(lambda x: tf.signal.frame(x, timesteps, 1, axis=0),
-                                          num_parallel_calls=tf.data.experimental.AUTOTUNE)
+    def audio_windows(self, index, timesteps):
+        self.examples = self.examples.map(
+            self._map_index(index, lambda x: tf.signal.frame(x, timesteps, 1, axis=0)),
+            num_parallel_calls=tf.data.experimental.AUTOTUNE)
         self.examples = self.examples.unbatch()
         return self
 
-    def audio_split(self, num_or_size_splits):
-        self.examples = self.examples.map(lambda x: tf.split(x, num_or_size_splits, 0),
-                                          num_parallel_calls=tf.data.experimental.AUTOTUNE)
+    def _map_index(self, index, fn):
+        def imap(*x):
+            return [fn(x[i]) if i == index else x[i] for i in range(len(x))]
+
+        return imap
+
+    def split(self, index, num_or_size_splits):
+        self.examples = self.examples.map(
+            self._map_index(index, lambda x: tf.split(x, num_or_size_splits, 0)),
+            num_parallel_calls=tf.data.experimental.AUTOTUNE)
         return self
 
-    def audio_stft(self):
-        self.examples = self.examples.map(lambda x: tf.reshape(x, [1, -1]),
-                                          num_parallel_calls=tf.data.experimental.AUTOTUNE)
-        self.examples = self.examples.map(lambda x: tf.signal.stft(x,
-                                                                   frame_length=1024,
-                                                                   frame_step=512,
-                                                                   fft_length=1024),
-                                          num_parallel_calls=tf.data.experimental.AUTOTUNE)
+    def stft(self, index, frame_length, hop_length, n_fft):
+        # self.examples = self.examples.map(
+        #     self._map_index(index, lambda x: tf.reshape(x, [1, -1])),
+        #     num_parallel_calls=tf.data.experimental.AUTOTUNE)
+        self.examples = self.examples.map(
+            self._map_index(index, lambda x: tf.signal.stft(x,
+                                                            frame_length=frame_length,
+                                                            frame_step=hop_length,
+                                                            fft_length=n_fft)),
+            num_parallel_calls=tf.data.experimental.AUTOTUNE)
         return self
 
-    def stft_magspec(self):
-        self.examples = self.examples.map(lambda x: tf.abs(x),
-                                          num_parallel_calls=tf.data.experimental.AUTOTUNE)
+    def abs(self, index):
+        self.examples = self.examples.map(
+            self._map_index(index, lambda x: tf.abs(x)),
+            num_parallel_calls=tf.data.experimental.AUTOTUNE)
         return self
 
-    def magspec_melspec(self):
-        self.examples = self.examples.map(lambda x: self._melspec(x),
-                                          num_parallel_calls=tf.data.experimental.AUTOTUNE)
+    def melspec(self, index, n_mels):
+        self.examples = self.examples.map(
+            self._map_index(index, self._melspec(n_mels)),
+            num_parallel_calls=tf.data.experimental.AUTOTUNE)
         return self
 
-    def _melspec(self, x):
-        num_spectrogram_bins = x.shape[-1]
+    def set_chan(self, index):
+        self.examples = self.examples.map(
+            self._map_index(index, lambda x: tf.reshape(x, [*x.shape, 1])),
+            num_parallel_calls=tf.data.experimental.AUTOTUNE)
+        return self
 
-        lower_edge_hertz, upper_edge_hertz, num_mel_bins = 80.0, 8000, 128
+    def _melspec(self, n_mels):
+        def mel(x):
+            num_spectrogram_bins = x.shape[-1]
 
-        linear_to_mel_weight_matrix = tf.signal.linear_to_mel_weight_matrix(
-            num_mel_bins, num_spectrogram_bins, self.sr, lower_edge_hertz,
-            upper_edge_hertz)
+            lower_edge_hertz, upper_edge_hertz, num_mel_bins = 80.0, 8000, n_mels
 
-        return tf.tensordot(x, linear_to_mel_weight_matrix, 1)
+            linear_to_mel_weight_matrix = tf.signal.linear_to_mel_weight_matrix(
+                num_mel_bins, num_spectrogram_bins, self.sr, lower_edge_hertz,
+                upper_edge_hertz)
+
+            return tf.tensordot(x, linear_to_mel_weight_matrix, 1)
+        return mel
+
+    def logspec(self, index):
+        self.examples = self.examples.map(
+            self._map_index(index, self._logspec),
+            num_parallel_calls=tf.data.experimental.AUTOTUNE)
+        return self
+
+    def _logspec(self, x):
+        amin = 1e-10
+        logspec = tf.math.log(tf.math.maximum(amin, x))
+        # logspec -= tf.math.log(tf.math.maximum(amin, ref))
+        return logspec
+
+    def flip_axes(self, index):
+        self.examples = self.examples.map(
+            self._map_index(index, lambda x: tf.transpose(x, [1, 0])),
+            num_parallel_calls=tf.data.experimental.AUTOTUNE)
+        return self
 
     def shuffle(self, buffer_size):
         self.examples = self.examples.shuffle(buffer_size)
         return self
 
     def batch(self, batch_size):
-        self.examples = self.examples.batch(batch_size)
+        self.examples = self.examples.batch(batch_size, drop_remainder=True)
+        return self
+
+    def unbatch(self):
+        self.examples = self.examples.unbatch()
         return self
 
     def prefetch(self):
--- src/nsynth/test.py
+++ src/nsynth/test.py
@@ -2,23 +2,32 @@ import tensorflow as tf
 from tensorflow.compat.v1 import ConfigProto
 from tensorflow.compat.v1 import InteractiveSession
 from tensorflow.keras import layers
+import librosa
 import matplotlib.pyplot as plt
 import numpy as np
 import time
+import random
 from scipy.io import wavfile
 from nsynth import NSynthDataset
-from model import create_model
+import model
+import threading
 
 config = ConfigProto()
 config.gpu_options.allow_growth = True
 session = InteractiveSession(config=config)
 
-buffer_size = 1024
-batch_size = 64
+buffer_size = 10_000
+batch_size = 32
 timesteps = 128
 channels = 1
+latent_size = 100
+frame_length = 4048
+hop_length = 2196
+n_fft = 4048
+samples = 64000
+n_mels = 28
 
-epochs = 100
+epochs = 100000
 
 def instrument_filter(x):
     return tf.reshape(tf.equal(x["instrument_family"], 0), [])
@@ -26,81 +35,156 @@ def instrument_filter(x):
 dataset = (NSynthDataset('/home/big/datasets/nsynth/nsynth-train.tfrecord')
            .dataset()
            .filter(instrument_filter)
-           .audio()
-           .audio_padstart(timesteps)
-           .audio_windows(timesteps+1)
-           .audio_split([timesteps, 1])
-           # .audio_stft()
-           # .stft_magspec()
-           # .magspec_melspec()
+           .extract(['audio'# , 'instrument_family'
+           ])
+           # .one_hot(1, 11)
+           .stft(0, frame_length, hop_length, n_fft)
+           .abs(0)
+           .melspec(0, n_mels)
+           # .logspec(0)
+           .flip_axes(0)
+           # .unbatch()
+           .set_chan(0)
+           # .set_chan(1)
+           .prefetch())
+
+fig, axs = plt.subplots(3)
+plt.ion()
+plt.show()
+
+for x, in dataset.examples.take(1):
+    shape = x.shape[0:2]
+
+dataset = (dataset
            .shuffle(buffer_size)
            .batch(batch_size)
            .prefetch())
 
-plt.ion()
-plt.show()
+print(shape)
+
+generator = model.create_generator(latent_size, batch_size, shape)
+discriminator = model.create_discriminator(shape)
+
+num_examples = 4
+
+seed = tf.random.normal([num_examples, latent_size])
+
+def concat_images(images):
+    return tf.concat([tf.reshape(image, shape) for image in tf.unstack(images, axis=0)], axis=1)
+
+def save_and_plot(epoch, save=False, batch=None):
+    if save:
+        print("Saving audio sample...")
+
+    image = concat_images(generator(seed, training=False))
+
+    if save:
+        print("Inverting melspecs...")
+        # image = tf.math.exp(image)
+        waveform = librosa.feature.inverse.mel_to_audio(
+            image.numpy(),
+            sr=dataset.sr,
+            n_fft=n_fft,
+            hop_length=hop_length,
+            win_length=frame_length)
+
+        print("Inverting done.")
 
 
-model = create_model(timesteps, channels)
-loss = tf.keras.losses.MeanSquaredError()
-optimizer = tf.keras.optimizers.Adam(1e-4)
 
+    if batch:
+        axs[1].cla()
+        axs[1].imshow(concat_images(batch[0][0:num_examples]).numpy())
 
-@tf.function
-def train_step(model, x, y):
-    with tf.GradientTape() as tape:
-        y_pred = model(x, training=True)
+    axs[0].cla()
+    axs[0].imshow(image.numpy())
 
-        batch_loss = loss(y_pred, y)
+    if save:
+        axs[2].cla()
+        print("Writing wav file...")
+        wavfile.write('audio/audio_{}_E{:04d}.wav'.format(int(time.time()*1000), epoch), dataset.sr, waveform)
+        axs[2].plot(waveform)
+        print("Writing wav file done.")
 
-    gradients = tape.gradient(batch_loss, model.trainable_variables)
-    optimizer.apply_gradients(zip(gradients, model.trainable_variables))
-
-    return batch_loss
-
-
-def generate_waveform(model, samples):
-    history = tf.unstack(tf.random.normal([1, timesteps, channels]), axis=1)
-    waveform = []
-
-    for i in range(samples):
-        histtens = tf.reshape(tf.concat(history, 0), [1, timesteps, channels])
-        y_pred = model(histtens, training=False)
-        del history[0]
-        history.append(y_pred)
-        waveform.append(y_pred)
-        if i % 1000 == 0:
-            print(f"Waveform sample {i}/{samples}")
-
-    return tf.concat(waveform, 0)
-
-
-def save_and_plot(epoch):
-    waveform = generate_waveform(model, 64000).numpy().reshape(-1)
-    wavfile.write('audio/audio_at_epoch_{:04d}.wav'.format(epoch), dataset.sr, waveform)
-
-    plt.clf()
-    plt.plot(waveform)
     plt.draw()
     plt.pause(0.1)
 
+    if save:
+        print("Save done.")
 
-for epoch in range(epochs):
-    epoch_loss_avg = tf.keras.metrics.Mean()
-    start = time.time()
 
-    step = 0
-    steps = 10000
-    for x, y in dataset.examples.take(steps):
-        batch_loss = train_step(model, x, y)
-        epoch_loss_avg(batch_loss)
-        step += 1
-        if step % 1000 == 0:
-            print (f'Loss: {epoch_loss_avg.result()} {int((step/steps)*100)}%')
+save_and_plot(0)
 
-    print (f'Loss: {epoch_loss_avg.result()}, Time for epoch {epoch+1} is {time.time()-start} sec')
 
-    if (epoch+1) % 10 == 0:
-        save_and_plot(epoch)
+cross_entropy = tf.keras.losses.BinaryCrossentropy(from_logits=True)
 
+generator_optimizer = tf.keras.optimizers.Adam(0.0001)
+discriminator_optimizer = tf.keras.optimizers.Adam(0.0004)
+
+
+def discriminator_loss(real_output, fake_output):
+    real_loss = cross_entropy(
+        tf.zeros_like(real_output)+tf.random.uniform(real_output.shape, 0, 0.1),
+        real_output)
+    fake_loss = cross_entropy(
+        tf.ones_like(fake_output)-tf.random.uniform(fake_output.shape, 0, 0.1),
+        fake_output)
+    total_loss = real_loss + fake_loss
+    return total_loss
+
+def generator_loss(fake_output):
+    return cross_entropy(tf.zeros_like(fake_output), fake_output)
+
+
+@tf.function
+def train_step(model, x):
+    noise = tf.random.normal([batch_size, latent_size])
+
+    with tf.GradientTape() as gen_tape, tf.GradientTape() as disc_tape:
+      generated_x = generator(noise, training=True)
+      print(generated_x.shape)
+
+      real_output = discriminator(x, training=True)
+      fake_output = discriminator(generated_x, training=True)
+
+      gen_loss = generator_loss(fake_output)
+      disc_loss = discriminator_loss(real_output, fake_output)
+
+    gradients_of_generator = gen_tape.gradient(gen_loss, generator.trainable_variables)
+    gradients_of_discriminator = disc_tape.gradient(disc_loss, discriminator.trainable_variables)
+
+    generator_optimizer.apply_gradients(zip(gradients_of_generator, generator.trainable_variables))
+    discriminator_optimizer.apply_gradients(zip(gradients_of_discriminator, discriminator.trainable_variables))
+
+    return gen_loss, disc_loss
+
+
+
+def train(epochs):
+    for epoch in range(epochs):
+        gen_loss_avg = tf.keras.metrics.Mean()
+        disc_loss_avg = tf.keras.metrics.Mean()
+        start = time.time()
+
+        step = 0
+        steps = 4000
+        for x in dataset.examples.take(steps):
+            gen_loss, disc_loss = train_step(model, x)
+            gen_loss_avg(gen_loss)
+            disc_loss_avg(disc_loss)
+            step += 1
+            if step % 10 == 0:
+                print (f'Loss gen: {gen_loss_avg.result()} loss disc: {disc_loss_avg.result()} {step}')
+                if step != steps:
+                    if step % 1000 == 0:
+                        save_and_plot(epoch, True)
+                    elif step % 20 == 0:
+                        save_and_plot(epoch, False, x)
+
+        print (f'Loss gen: {gen_loss_avg.result()} loss disc: {disc_loss_avg.result()}, Time for epoch {epoch+1} is {time.time()-start} sec')
+
+        save_and_plot(epoch, True)
+        # elif epoch % 2 == 0:
+
+train(epochs)
 
