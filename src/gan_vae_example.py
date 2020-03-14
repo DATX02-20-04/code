0e18fb31e1bbeddbf24fd6a5221abf6b91f8e476
Elias Sundqvist <elias@sundqvist.com>
2020-03-14 18:00:33 +0100

    [WIP] Add VAE GAN model

--- src/gan_vae_example.py
+++ src/gan_vae_example.py
@@ -1,14 +1,15 @@
 import tensorflow as tf
+import tensorflow_probability as tfp
 import preprocess
 import os
 from losses import create_simple_gan_loss
-from training import Trainer, create_gan_train_step
+from training import Trainer, create_gan_train_step, create_vae_gan_train_step
 from datasets.nsynth import nsynth_from_tfrecord, instruments, nsynth_to_melspec
-from models.simple_gan import create_generator, create_discriminator
+from models.gan_vae import create_gan_vae
 import matplotlib.pyplot as plt
 
 import argparse
-#import IPython.display as display
+# import IPython.display as display
 from model import Model
 import tensorflow_datasets as tfds
 
@@ -17,29 +18,46 @@ from tensorflow.compat.v1 import ConfigProto
 from tensorflow.compat.v1 import InteractiveSession
 import tensorflow_datasets as tfds
 
+tfpl = tfp.layers
+tfd = tfp.distributions
+
 config = ConfigProto()
 config.gpu_options.allow_growth = True
 session = InteractiveSession(config=config)
 
 
-class GANExample(Model):
+class GANVAEExample(Model):
     def __init__(self, dataset, hparams):
-        super(GANExample, self).__init__(dataset, hparams)
+        super(GANVAEExample, self).__init__(dataset, hparams)
 
         # Create the generator and discriminator loss functions
-        self.generator_loss, self.discriminator_loss = create_simple_gan_loss(tf.keras.losses.BinaryCrossentropy(from_logits=True))
+        self.generator_loss, self.discriminator_loss = create_simple_gan_loss(
+            tf.keras.losses.BinaryCrossentropy(from_logits=True))
 
         # Define the optimizers
         self.generator_optimizer = tf.keras.optimizers.Adam(self.hparams['gen_lr'])
         self.discriminator_optimizer = tf.keras.optimizers.Adam(self.hparams['disc_lr'])
+        self.vae_optimizer = tf.keras.optimizers.Adam(self.hparams['vae_lr'])
+
+        self.prior = tfd.Independent(tfd.Normal(loc=tf.zeros((self.hparams['latent_size'])), scale=1),
+                                     reinterpreted_batch_ndims=1)
+        self.vae_loss = lambda x, rv_x: -rv_x.log_prob(x)
+        self.latent_loss = tf.keras.losses.MSE
 
         # Create the actual models for the generator and discriminator
-        self.generator = create_generator(self.hparams['latent_size'], self.hparams['generator_scale'], self.spec_shape)
-        self.discriminator = create_discriminator(self.spec_shape)
+        self.vae, self.encoder, self.decoder, self.generator, self.discriminator = create_gan_vae(self.spec_shape,
+                                                                                                  self.hparams[
+                                                                                                       'latent_size'],
+                                                                                                  self.hparams[
+                                                                                                       'gen_z_size'],
+                                                                                                  self.hparams[
+                                                                                                       'model_scale'],
+                                                                                                  self.prior)
 
         # Define some metrics to be used in the training
         self.gen_loss_avg = tf.keras.metrics.Mean()
         self.disc_loss_avg = tf.keras.metrics.Mean()
+        self.vae_loss_avg = tf.keras.metrics.Mean()
 
         if 'save_dir' in self.hparams:
             self.image_save_dir = os.path.join(self.hparams['save_dir'], 'images/')
@@ -50,24 +68,34 @@ class GANExample(Model):
 
         self.seed = tf.random.normal([self.hparams['num_examples'], self.hparams['latent_size']])
 
-
         self.ckpt = tf.train.Checkpoint(
             step=self.trainer.step,
             generator=self.generator,
             discriminator=self.discriminator,
+            vae=self.vae,
+            encoder=self.encoder,
+            decoder=self.decoder,
             gen_optimizer=self.generator_optimizer,
             disc_optimizer=self.discriminator_optimizer,
+            vae_optimizer=self.vae_optimizer,
         )
 
+
         self.trainer.init_checkpoint(self.ckpt)
-        self.trainer.set_train_step(create_gan_train_step(self.generator,
-                                                          self.discriminator,
-                                                          self.generator_loss,
-                                                          self.discriminator_loss,
-                                                          self.generator_optimizer,
-                                                          self.discriminator_optimizer,
-                                                          self.hparams['batch_size'],
-                                                          self.hparams['latent_size']))
+        self.trainer.set_train_step(create_vae_gan_train_step(self.generator,
+                                                              self.discriminator,
+                                                              self.encoder,
+                                                              self.decoder,
+                                                              self.vae,
+                                                              self.generator_loss,
+                                                              self.discriminator_loss,
+                                                              self.vae_loss,
+                                                              self.latent_loss,
+                                                              self.generator_optimizer,
+                                                              self.discriminator_optimizer,
+                                                              self.vae_optimizer,
+                                                              self.hparams['batch_size'],
+                                                              self.hparams['gen_z_size']))
 
     def instrument_filter(self, x):
         return tf.reshape(tf.math.equal(x['instrument_family'], instruments[self.hparams['instrument']]), [])
@@ -84,7 +112,8 @@ class GANExample(Model):
 
         # Make sure the dimensions of spectogram is divisible by 4.
         # This is because the generator is going to upscale it's state twice with a factor of 2.
-        assert self.spec_shape[0] % 4 == 0 and self.spec_shape[1] % 4 == 0, "Spectogram dimensions is not divisible by 4"
+        assert self.spec_shape[0] % 4 == 0 and self.spec_shape[1] % 4 == 0, \
+            "Spectogram dimensions is not divisible by 4"
 
         # Create preprocessing pipeline for shuffling and batching
         return preprocess.pipeline([
@@ -94,33 +123,35 @@ class GANExample(Model):
             preprocess.prefetch()
         ])(dataset)
 
-
     # This runs at the start of every epoch
     def on_epoch_start(self, epoch, step):
         self.gen_loss_avg = tf.keras.metrics.Mean()
         self.disc_loss_avg = tf.keras.metrics.Mean()
+        self.vae_loss_avg = tf.keras.metrics.Mean()
 
     # This runs at every step in the training (for each batch in dataset)
     def on_step(self, step, stats):
-        gen_loss, disc_loss = stats
+        gen_loss, disc_loss, vae_loss = stats
         self.gen_loss_avg(gen_loss)
         self.disc_loss_avg(disc_loss)
+        self.vae_loss_avg(vae_loss)
         if step % 100 == 0:
-            print(f"Step: {step}, Gen Loss: {self.gen_loss_avg.result()}, Disc Loss: {self.disc_loss_avg.result()}")
+            print(f"Step: {step}, Gen Loss: {self.gen_loss_avg.result()}, Disc Loss: {self.disc_loss_avg.result()}, VAE Loss: {self.vae_loss_avg.result()}")
 
     # This runs at the end of every epoch and is used to display metrics
     def on_epoch_complete(self, epoch, step, duration):
-        #display.clear_output(wait=True)
-        print(f"Epoch: {epoch}, Step: {step}, Gen Loss: {self.gen_loss_avg.result()}, Disc Loss: {self.disc_loss_avg.result()}, Duration: {duration} s")
+        # display.clear_output(wait=True)
+        print(
+            f"Epoch: {epoch}, Step: {step}, Gen Loss: {self.gen_loss_avg.result()}, Disc Loss: {self.disc_loss_avg.result()}, VAE Loss: {self.vae_loss_avg.result()}, Duration: {duration} s")
         self.generate_and_save_images_epoch(epoch, step)
 
     def generate_and_save_images_epoch(self, epoch, step):
         generated = self.generator(self.seed, training=False)
 
-        fig = plt.figure(figsize=(4,4))
+        fig = plt.figure(figsize=(4, 4))
 
         for i in range(generated.shape[0]):
-            plt.subplot(4, 4, i+1)
+            plt.subplot(4, 4, i + 1)
             plt.imshow(generated[i, :, :, 0])
             plt.axis('off')
 
@@ -136,10 +167,10 @@ class GANExample(Model):
         generated = self.generator(seed, training=False)
 
         print("Generated melspec samples:")
-        fig = plt.figure(figsize=(4,4))
+        fig = plt.figure(figsize=(4, 4))
 
         for i in range(generated.shape[0]):
-            plt.subplot(4, 4, i+1)
+            plt.subplot(4, 4, i + 1)
             plt.imshow(generated[i, :, :, 0])
             plt.axis('off')
 
@@ -169,9 +200,11 @@ if __name__ == '__main__':
         'batch_size': 32,
         'buffer_size': 1000,
         'latent_size': 100,
-        'generator_scale': 128,
+        'model_scale': 128,
         'gen_lr': 0.0001,
         'disc_lr': 0.0004,
+        'vae_lr': 0.0001,
+        'gen_z_size': 16,
         'log_amin': 1e-5,
         'num_examples': 16,
         'save_dir': './',
@@ -182,5 +215,5 @@ if __name__ == '__main__':
     dataset = tfds.load('nsynth/gansynth_subset', split='train', shuffle_files=True)
 
     dataset = nsynth_to_melspec(dataset, hparams)
-    gan = GANExample(dataset, hparams)
+    gan = GANVAEExample(dataset, hparams)
     gan.train()
