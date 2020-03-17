062c24c1d0efea3ac0cb664bb33faf1209968e96
Christoffer Arvidsson <christoffer@arvidson.nu>
2020-03-17 15:52:20 +0100

    Remove print

--- src/util.py
+++ src/util.py
@@ -4,7 +4,6 @@ def load_hparams(path):
     with open(path, 'r') as stream:
         try:
             defaults = yaml.safe_load(stream)['hparams']
-            print(defaults)
         except yaml.YAMLError as e:
             print(e)
 
