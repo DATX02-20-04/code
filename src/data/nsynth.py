dd8c7d592e950c2030d6b0b036b06a7610ec9b30
Eric Norman <eric.norman98@hotmail.se>
2020-03-18 22:17:29 +0100

    Fix keyerror @Caagr98

--- src/data/nsynth.py
+++ src/data/nsynth.py
@@ -16,7 +16,7 @@ def nsynth_from_tfrecord(nsynth_tfrecord_path):
     ])(dataset)
 
 def nsynth_to_melspec(dataset, hparams):
-    if hparams['instrument'] is not None:
+    if 'instrument' in hparams and hparams['instrument'] is not None:
         dataset = pro.filter_transform(instrument_filter)(dataset)
 
     # Create preprocessing pipeline for the melspectograms
