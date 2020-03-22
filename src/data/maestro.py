82881e84ad74663ffc28da284fe5de73e05d6ba7
Christoffer Arvidsson <christoffer@arvidson.nu>
2020-03-22 18:35:46 +0100

    Add maestro_to_melspec function

--- src/data/maestro.py
+++ src/data/maestro.py
@@ -14,3 +14,16 @@ def maestro_from_files(root_path, frame_size):
     ])(dataset)
 
     return dataset
+
+
+def maestro_to_melspec(dataset, hparams):
+    # Create preprocessing pipeline for the melspectograms
+    return pro.pipeline([
+        pro.extract('audio_filename'),  # Get path to wav file
+        pro.read_file(),  # Read wav file
+        pro.decode_wav(),  # Decode wav file
+        pro.melspec(sr=hparams['sample_rate']),
+        pro.pad([[0, 0], [0, 4]], 'CONSTANT', constant_values=hparams['log_amin']),
+        pro.amp_to_log(amin=hparams['log_amin']),
+        pro.normalize(),
+    ])(dataset)
