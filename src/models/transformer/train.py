9745604b2906f5f14850eaaa84a658fa82309b3a
Eric Norman <eric.norman98@hotmail.se>
2020-03-24 10:06:11 +0100

    Add genetic optimization

--- src/models/transformer/train.py
+++ src/models/transformer/train.py
@@ -7,10 +7,13 @@ import data.process as pro
 from models.transformer.model import Transformer
 import tensorflow_datasets as tfds
 from evolve.hparams import HParams
-
+from evolve.pool import Pool
 
 train_loss = tfk.metrics.Mean(name='train_loss')
 train_accuracy = tfk.metrics.SparseCategoricalAccuracy(name='train_accuracy')
+
+input_vocab_size  = 128+128+100+100
+target_vocab_size = 128+128+100+100
 
 # This runs at the start of every epoch
 def on_epoch_start(epoch, step):
@@ -30,11 +33,29 @@ def on_epoch_complete(epoch, step, duration):
     #display.clear_output(wait=True)
     print(f"Epoch: {epoch}, Step: {step}, Loss: {train_loss.result()}, Accuracy: {train_accuracy.result()}, Duration: {duration:.3f}")
 
+def create_model(hp):
+    return Transformer(input_vocab_size=input_vocab_size,
+                       target_vocab_size=target_vocab_size,
+                       pe_input=input_vocab_size,
+                       pe_target=target_vocab_size,
+                       hparams=hparams)
+
+def evaluate(dataset, hparams):
+    def _eval(model):
+        trainer = Trainer(dataset, hparams)
+
+        trainer.set_train_step(model.train_step)
+        trainer.on_epoch_start = on_epoch_start
+        trainer.on_step = on_step
+        trainer.on_epoch_complete = on_epoch_complete
+
+        loss, _, _ = trainer.run()
+        return tf.reduce_mean(loss)
+    return _eval
+
 def start(hparams):
-    hparams = HParams(hparams).generate()
     print(hparams)
-    input_vocab_size  = 128+128+100+100
-    target_vocab_size = 128+128+100+100
+    pool = Pool(create_model, hparams)
 
     dataset = tf.data.Dataset.list_files(os.path.join(hparams['dataset_root'] if 'dataset_root' in hparams else './maestro-v2.0.0/', '**/*.midi'))
 
@@ -61,29 +82,23 @@ def start(hparams):
         pro.prefetch(),
     ])(dataset_single)
 
-    dataset_single = dataset_single.as_numpy_iterator()
+    pop_size = 10
+    generations = 10
+    mutation_rate = 0.2
+    pool.populate(pop_size, 1)
 
-    transformer = Transformer(input_vocab_size=input_vocab_size,
-                              target_vocab_size=target_vocab_size,
-                              pe_input=input_vocab_size,
-                              pe_target=target_vocab_size,
-                              hparams=hparams)
+    for generation in range(generations):
+        (pool
+         .evaluate(evaluate(dataset, hparams))
+         .select(pop_size)
+         .populate(pop_size, mutation_rate))
 
+    print("BEST:", pool.best)
 
+    # ckpt = tf.train.Checkpoint(
+    #     step=trainer.step,
+    #     transformer=transformer,
+    #     optimizer=transformer.optimizer
+    # )
 
-    seed = next(dataset_single)
-
-    trainer = Trainer(dataset, hparams)
-    ckpt = tf.train.Checkpoint(
-        step=trainer.step,
-        transformer=transformer,
-        optimizer=transformer.optimizer
-    )
-
-    trainer.init_checkpoint(ckpt)
-    trainer.set_train_step(transformer.train_step)
-    trainer.on_epoch_start = on_epoch_start
-    trainer.on_step = on_step
-    trainer.on_epoch_complete = on_epoch_complete
-
-    trainer.run()
+    # trainer.init_checkpoint(ckpt)
