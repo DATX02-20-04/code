7574d830c4ad660de4abe2b03f1e90dce324aa34
Eric Norman <eric.norman98@hotmail.se>
2020-03-31 18:40:23 +0200

    Change to multilayer thing

--- src/models/vae/model.py
+++ src/models/vae/model.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+import math
 import tensorflow_probability as tfp
 from tensorflow_probability import layers as tfpl
 from tensorflow_probability import distributions as tfd
@@ -30,48 +31,40 @@ class VAE():
         return loss
 
     def create_vae(self):
-        i = tfkl.Input(shape=(self.hparams['window_samples'], 1))
+        input_size = self.hparams['window_samples']
+        scale = self.hparams['model_scale']
+        n_layers = 4
 
-        o = tfkl.Conv1D(self.hparams['model_scale'], kernel_size=32, strides=256, dilation_rate=1)(i)
-        o = tfkl.BatchNormalization(axis=1)(o)
-        o = tfkl.ReLU()(o)
-
-        o = tfkl.Conv1D(2 * self.hparams['model_scale'], kernel_size=16, strides=16)(o)
-        o = tfkl.BatchNormalization(axis=1)(o)
-        o = tfkl.ReLU()(o)
+        i = tfkl.Input(shape=(input_size, 1))
+        o = i
+        for n in range(1,n_layers+1):
+            o = tfkl.Conv1D(scale*(n+1), kernel_size=2, strides=2, padding='same')(o)
+            o = tfkl.BatchNormalization(axis=1)(o)
+            o = tfkl.ReLU()(o)
 
         o = tfkl.Flatten()(o)
 
-        o = tfkl.Dense(tfpl.MultivariateNormalTriL.params_size(self.hparams['latent_size']), activation=None)(o)
-        o = tfpl.MultivariateNormalTriL(self.hparams['latent_size'], activity_regularizer=tfpl.KLDivergenceRegularizer(self.prior))(o)
+        o = tfkl.Dense(tfpl.IndependentNormal.params_size(self.hparams['latent_size']), activation=None)(o)
+        o = tfpl.IndependentNormal(self.hparams['latent_size'], activity_regularizer=tfpl.KLDivergenceRegularizer(self.prior, weight=2.0))(o)
 
         encoder = tfk.Model(inputs=i, outputs=o)
 
-        s = self.hparams['window_samples'] // 4
-
         i = tfkl.Input(shape=(self.hparams['latent_size'],))
-
+        s = scale*(n_layers+1)
         o = tfkl.Dense(s, activation='relu')(i)
-        o = tfkl.Reshape(target_shape=(s, 1))(o)
+        o = tfkl.Reshape(target_shape=(1, s))(o)
 
-        o = tfkl.UpSampling1D(size=2)(o)
-        o = tfkl.Conv1D(2 * self.hparams['model_scale'], kernel_size=3, strides=1, padding='SAME')(o)
-        o = tfkl.BatchNormalization(axis=1)(o)
-        o = tfkl.ReLU()(o)
+        for n in range(1,n_layers+1):
+            o = tfkl.UpSampling1D(size=2)(o)
+            o = tfkl.Conv1D(scale*(n_layers+1-n), kernel_size=2, strides=1, padding='same')(o)
+            o = tfkl.BatchNormalization(axis=1)(o)
+            o = tfkl.ReLU()(o)
 
-        o = tfkl.UpSampling1D(size=2)(o)
-        o = tfkl.Conv1D(self.hparams['model_scale'], kernel_size=3, strides=1, padding='SAME')(o)
-        o = tfkl.BatchNormalization(axis=1)(o)
-        o = tfkl.ReLU()(o)
-
-        o = tfkl.Conv1D(1, kernel_size=3, strides=1, padding='SAME')(o)
-        o = tfkl.BatchNormalization(axis=1)(o)
-        o = tfkl.ReLU()(o)
-
+        o = tfkl.Cropping1D((0,o.shape[1]-input_size))(o)
+        o = tfkl.Conv1D(1, kernel_size=1, strides=1, padding='same')(o)
         o = tfkl.Flatten()(o)
 
         o = tfkl.Dense(tfpl.IndependentNormal.params_size((self.hparams['window_samples'], 1)), activation='tanh')(o)
-
         o = tfpl.IndependentNormal((self.hparams['window_samples'],1))(o)
 
         decoder = tfk.Model(inputs=i, outputs=o)
