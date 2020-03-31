e012ce1c6dd14ffda22586ce126922b37189dcdd
Eric Norman <eric.norman98@hotmail.se>
2020-03-31 15:47:01 +0200

    Move tensorboard code to trainer

--- src/models/common/training.py
+++ src/models/common/training.py
@@ -1,12 +1,20 @@
 import tensorflow as tf
 import os
 import time
+import datetime
 
 class Trainer():
     def __init__(self, dataset, hparams):
         self.dataset = dataset
         self.hparams = hparams
         self.step = tf.Variable(0)
+        self.train_summary_writer = None
+
+    def init_tensorboard(self):
+        # Tensorfboard logging
+        current_time = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+        train_log_dir = f"./logs/{self.hparams['name']}/{current_time}/train/"
+        self.train_summary_writer = tf.summary.create_file_writer(train_log_dir)
 
     def init_checkpoint(self, ckpt):
         self.ckpt = ckpt
@@ -23,13 +31,13 @@ class Trainer():
     def set_train_step(self, train_step):
         self.train_step = train_step
 
-    def on_epoch_start(self, epoch, step):
+    def on_epoch_start(self, epoch, step, tsw=None):
         pass
 
-    def on_step(self, epoch, step, stats):
+    def on_step(self, epoch, step, stats, tsw=None):
         pass
 
-    def on_epoch_complete(self, epoch, step, duration):
+    def on_epoch_complete(self, epoch, step, duration, tsw=None):
         print(f'Epoch {epoch} completed in {duration:.3f} seconds at step {step}')
 
     def run(self):
@@ -40,18 +48,18 @@ class Trainer():
 
         for epoch in range(1, self.hparams['epochs']+1):
             start = time.time()
-            self.on_epoch_start(epoch, self.step.numpy())
+            self.on_epoch_start(epoch, self.step.numpy(), tsw=self.train_summary_writer)
 
             d = self.dataset.take(steps_per_epoch) if steps_per_epoch > 0 else self.dataset
 
             for batch in d:
                 self.step.assign_add(1)
                 stats = self.train_step(batch)
-                self.on_step(epoch, self.step.numpy(), stats)
+                self.on_step(epoch, self.step.numpy(), stats, tsw=self.train_summary_writer)
 
             if self.ckpt is not None:
                 self.manager.save()
 
             end = time.time()
             duration = end - start
-            self.on_epoch_complete(epoch, self.step.numpy(), duration)
+            self.on_epoch_complete(epoch, self.step.numpy(), duration, tsw=self.train_summary_writer)
