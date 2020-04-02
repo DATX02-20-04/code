0ae007eaf92d48708ae973baa833917c0afbc0ce
Christoffer Arvidsson <christoffer@arvidson.nu>
2020-04-02 19:54:58 +0200

    Test spectrogram inverse

---
+++ src/test/spec_inverse.py
@@ -0,0 +1,32 @@
+import tensorflow as tf
+import data.process as pro
+from data.nsynth import nsynth_to_melspec
+import matplotlib.pyplot as plt
+import librosa
+
+
+dataset = tf.data.Dataset.list_files('src/audio')
+
+
+hparams = {
+    'sample_rate': 16000,
+    'log_amin': 1e-6,
+    'instrument': 'keyboard',
+}
+
+dataset = nsynth_to_melspec(dataset, hparams)
+
+dataset = pro.pipeline([
+    pro.melspec(hparams['sample_rate']),
+    pro.numpy(),
+])(dataset)
+
+x = next(dataset)
+plt.imshow(x)
+
+dataset = pro.pipeline([
+    pro.invert_log_melspec(hparams['sample_rate'])
+])(dataset)
+
+x = next(dataset)
+librosa.output.write_wav('result.wav', x, hparams['sample_rate'])
