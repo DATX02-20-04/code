14e18416cd1ea974c6ea7cb26e5bfb894164cc46
training computer for cool ai music group <computer@coolaimusic.com>
2020-04-10 10:45:32 +0200

    Prevent processing entire dataset for stats, load instead

--- src/models/gan/train.py
+++ src/models/gan/train.py
@@ -33,28 +33,13 @@ def on_step(epoch, step, stats, tsw):
     if step % 100 == 0:
         print(f"Epoch: {epoch}, Step: {step}, Gen Loss: {gen_loss_avg.result()}, Disc Loss: {disc_loss_avg.result()}")
 
-# This runs at the end of every epoch and is used to display metrics
-def on_epoch_complete(epoch, step, duration, tsw, gan):
-    #display.clear_output(wait=True)
-    count = 5
-    seed = tf.random.normal((count, gan.hparams['latent_size']))
-    mid = gan.hparams['cond_vector_size']//2
-    pitches = tf.one_hot(range(mid-count//2, mid+count//2), gan.hparams['cond_vector_size'], axis=1)
-
-    samples = tf.reshape(gan.generator([seed, pitches], training=False), [-1, 128, 128])
-    img = tf.unstack(samples)
-    img = tf.reverse(tf.concat(img, axis=1), axis=[0])
-
-    with tsw.as_default():
-        tf.summary.image(f'Spectrogram at epoch {epoch}', img, step=step)
-    print(f"Epoch: {epoch}, Step: {step}, Gen Loss: {gen_loss_avg.result()}, Disc Loss: {disc_loss_avg.result()}, Duration: {duration} s")
-
 def start(hparams):
 
     # Load nsynth dataset from tfds
     dataset = tfds.load('nsynth/gansynth_subset', split='train', shuffle_files=True)
 
-    gan_stats = calculate_dataset_stats(hparams, dataset)
+    #gan_stats = calculate_dataset_stats(hparams, dataset)
+    gan_stats = np.load('gan_stats.npz')
 
     dataset = nsynth_to_melspec(dataset, hparams, gan_stats)
 
