5f7ed233cf90ef322e0f1aa3eb70f5536870696f
Christoffer Arvidsson <christoffer@arvidson.nu>
2020-04-11 16:38:20 +0200

    Make inverse spec test mimic our pipeline

--- src/test/spec_inverse.py
+++ src/test/spec_inverse.py
@@ -3,9 +3,11 @@ import data.process as pro
 from data.nsynth import nsynth_to_melspec
 import matplotlib.pyplot as plt
 import librosa
+import numpy as np
 
 
 dataset = tf.data.Dataset.list_files('src/audio/*.wav')
+stats = np.load('gan_stats')
 
 
 hparams = {
@@ -16,6 +18,8 @@ hparams = {
 dataset = pro.pipeline([
     pro.wav(),
     pro.melspec(hparams['sample_rate']),
+    pro.pad([[0, 0], [0, 2]], 'CONSTANT', constant_values=hparams['log_amin']),
+    pro.normalize(normalization='specgan', stats=stats),
     pro.numpy(),
 ])(dataset)
 x = next(dataset)
@@ -23,6 +27,7 @@ plt.imshow(x)
 plt.savefig('result.png')
 
 dataset = pro.pipeline([
+    pro.denormalize(normalization='specgan', stats=stats),
     pro.invert_log_melspec(hparams['sample_rate'])
 ])(dataset)
 
