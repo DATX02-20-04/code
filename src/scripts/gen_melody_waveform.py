833194993db6f96a0b5a8cc6d60ca254226c4103
Elias Sundqvist <elias@sundqvist.com>
2020-04-14 18:04:32 +0200

    Add description for melody waveform generation script

---
+++ src/scripts/gen_melody_waveform.py
@@ -0,0 +1,4 @@
+# This script should be used to generate a waveform by first running the music
+# transformer and then generating each individual note with specgan.
+# This is then combined into a single wavfile. 
+
