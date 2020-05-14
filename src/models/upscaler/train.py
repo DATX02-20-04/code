18ed1316cfb889d26f9d0cb9c5d991c5c0b3cc57
Eric Norman <eric.norman98@hotmail.se>
2020-05-14 09:31:34 +0200

    Log separate

--- src/models/upscaler/train.py
+++ src/models/upscaler/train.py
@@ -23,7 +23,9 @@ class TFImageCallback(tf.keras.callbacks.Callback):
             XYP = tf.concat([tf.squeeze(X), tf.squeeze(Y), tf.squeeze(P)], axis=0)
 
             with self.tbw.as_default():
-                tf.summary.image(f'XYP', XYP, step=self.step)
+                tf.summary.image(f'X', XYP[0], step=self.step)
+                tf.summary.image(f'Y', XYP[1], step=self.step)
+                tf.summary.image(f'P', XYP[2], step=self.step)
 
             self.step += 1
 
