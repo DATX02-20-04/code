d68710163abe46e4277058d9a2089b83b28db867
Eric Norman <eric.norman98@hotmail.se>
2020-05-14 09:56:02 +0200

    Remove reshape

--- src/models/upscaler/train.py
+++ src/models/upscaler/train.py
@@ -52,8 +52,6 @@ def start(hparams):
     valid = stats['examples']//10
     train = stats['examples']-valid
 
-    dataset = dataset.map(lambda x, y: (tf.reshape(x, [128, 256]), y))
-
     dataset = dataset.shuffle(1000)
     valid_dataset = dataset.take(valid)
     valid_dataset = dataset.batch(8, drop_remainder=True)
